c5eb70412b454b3832c94f020fd66f79e349f3d0
Brishti Shil <brishtishil62@gmail.com>
2022-05-14 14:55:13 +0600

    create doctors portal

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import Banner from './Banner/Banner';
+import Contact from './Contact';
+import Description from './Description';
+import Footer from '../Shared/Footer';
+import Info from './Info';
+import MakeAppointment from './MakeAppointment';
+import Services from './Services';
+import Testimonial from './Testimonial';
+
 
 const Home = () => {
     return (
         <div>
-            <h2>This is home</h2>
+            <Banner></Banner>
+            <Info></Info>
+            <Services></Services>
+            <Description></Description>
+            <MakeAppointment></MakeAppointment>
+            <Testimonial></Testimonial>
+            <Contact></Contact>
+            <Footer></Footer>
         </div>
     );
 };
---
+++ src/Pages/Shared/Loading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loading = () => {
+    return (
+        <div className=' flex justify-center items-center'>
+            <button className="w-16 h-16 border-b-2 border-gray-900 rounded-full animation-spin">loading</button>
+        </div>
+    );
+};
+
+export default Loading;
